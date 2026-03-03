c8330a2cd32b6a14ff359140f43613babca24d93
Poojith P <poojithpagadekal26@gmail.com>
2026-03-03 13:42:54 +0530

    feat: add GET /api/jobs/:jobId endpoint to retrieve job details

--- src/modules/job/job.route.ts
+++ src/modules/job/job.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createJobHandler,
+  getJobByIdHandler,
   getJobHandler,
   updateJobHandler,
 } from "./job.controller";
@@ -11,6 +12,7 @@ const jobRouter = express.Router();
 
 jobRouter.post("/",reportLimiter, validate(CreateJobSchema), createJobHandler);
 jobRouter.get("/", getJobHandler);
+jobRouter.get("/:jobId",validate(UpdateJobSchema),getJobByIdHandler)
 jobRouter.patch("/:jobId", validate(UpdateJobSchema), updateJobHandler);
 
 export default jobRouter;
