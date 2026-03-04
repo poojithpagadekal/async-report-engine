56a996c98177b75b7aec28a63a8487fa1e1958e9
Poojith P <poojithpagadekal26@gmail.com>
2026-03-04 10:20:55 +0530

    refactor:run worker alongside api in same container

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 echo "Running database migrations..."
 npx prisma migrate deploy
 
+echo "Starting worker in background..."
+node dist/workers/worker-server.js &
+
 echo "Starting server..."
 exec node dist/server.js
